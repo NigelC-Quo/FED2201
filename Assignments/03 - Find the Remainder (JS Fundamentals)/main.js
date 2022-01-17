2c0503a1edbf5592d24b81f67b74896dad4d17bf
NigelC-Quo <nigelclayton2@gmail.com>
2022-01-17 15:26:57 -0500

    Added the code to the Remainder function

---
+++ Assignments/03 - Find the Remainder (JS Fundamentals)/main.js
@@ -0,0 +1,26 @@
+function RemainderFunc(dividend, divisor) {
+
+    /*Create a function that will take any number, 
+    divide the number by any number, and the output 
+    should be how many times the number got divided 
+    as well as the modulus of the numbers you are dividing.*/
+
+    var count = 0;
+
+    if (divisor == 0) {
+
+        console.log("Error: Cannot Divide By Zero")
+    }
+     
+    else if (divisor > 0) {
+
+        for (let i = 1; divisor <= dividend; i++) {
+
+            count = i;
+
+            dividend -= divisor;
+        }
+
+        return console.log("Result: [" + count + "] Remainder: [" + dividend + "]");
+    }
+}
