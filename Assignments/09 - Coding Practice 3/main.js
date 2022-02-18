0ad43efb80a5750b6f0f56ff9342d3a3c6e7f731
NigelC-Quo <nigelclayton2@gmail.com>
2022-02-18 15:55:21 -0500

    Added js code for days between two dates

--- Assignments/09 - Coding Practice 3/main.js
+++ Assignments/09 - Coding Practice 3/main.js
@@ -1,10 +1,14 @@
 var CC9 = document.getElementsByClassName("CC9");
 var CC10 = document.getElementsByClassName("CC10");
+
 var inputDateTo2030 = document.getElementById("2030ID");
-var daysBetween = document.getElementById("between2Dates");
 var fromTodayBtn = document.getElementById("2030Btn");
+
+var daysBetween1 = document.getElementById("date1");
+var daysBetween2 = document.getElementById("date2");
+
 const date2030 = new Date("01/01/2030");
-var today = new Date();
+const today = new Date();
 
 
 fromTodayBtn.addEventListener('click', e => {
@@ -12,14 +16,15 @@ fromTodayBtn.addEventListener('click', e => {
 
     todayTo2030()
     dateTo2030()
+    twoDates()
 });
 
 function todayTo2030() {
 
     let differenceInTime = date2030.getTime() - today.getTime();
-    let differenceInDays = differenceInTime / (1000 * 3600 * 24);
+    let differenceInDays = Math.round(differenceInTime / (1000 * 3600 * 24));
 
-    console.log(differenceInDays)
+    console.log(`Today, there are ${differenceInDays} days until the year 2030`)
 }
 
 function dateTo2030() {
@@ -27,29 +32,25 @@ function dateTo2030() {
     let diff = new Date(inputDateTo2030.value)
 
     let differenceInTime = date2030.getTime() - diff.getTime()
-    let differenceInDays = differenceInTime / (1000 * 3600 * 24)
+    let differenceInDays = Math.round(differenceInTime / (1000 * 3600 * 24))
 
-    console.log(differenceInDays)
+    console.log(`There are ${differenceInDays} days until the year 2030 from this date`)
 }
 
 function twoDates() {
-    
+
     // JavaScript program to illustrate
     // calculation of no. of days between two date
-    
+
     // To set two dates to two variables
-    var date1 = new Date("02/18/2022");
-    var date2 = new Date("02/18/2023");
-    
+    let date1 = new Date(daysBetween1.value);
+    let date2 = new Date(daysBetween2.value);
+
     // To calculate the time difference of two dates
     var differenceInTime = date2.getTime() - date1.getTime();
-    
+
     // To calculate the no. of days between two dates
-    var differenceInDays = differenceInTime / (1000 * 3600 * 24);
-    
-    //To display the final no. of days (result)
-    document.write("Total number of days between dates <br>" +
-        date1 + "<br> and <br>" +
-        date2 + " is: <br> " +
-        differenceInDays);
+    var differenceInDays = Math.abs(differenceInTime / (1000 * 3600 * 24));
+
+    console.log(`There are ${differenceInDays} days between these dates`)
 }
