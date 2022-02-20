f202569d5b4a9be51ee8c092d41a6e98257250f2
NigelC-Quo <nigelclayton2@gmail.com>
2022-02-20 16:30:01 -0500

    Added theme toggle functionality, ability to remove certain list items, and made so a list item can be completed

--- Assignments/08 - ToDo App/main.js
+++ Assignments/08 - ToDo App/main.js
@@ -1,4 +1,62 @@
 $(document).ready(() => {
+    var backImage = $(".bgImg");
+    var list = $("#toDoList");
+    var active = $(".incomplete");
+    var completed = $(".completed")
+    var entry = $("#toDoEntry");
+    var mode = $(".theme")
+    var darkMode = false;
+    var finished = false;
+    var removeItem = $(".cross")
 
-    
+    // Dark theme toggle
+    function darkTheme() {
+        $(backImage).attr("src", "images/bg-mobile-dark.jpg");
+        $(mode).attr("src", "images/icon-sun.svg");
+
+        darkMode = true;
+    }
+    // Light theme toggle
+    function lightTheme() {
+        $(backImage).attr("src", "images/bg-mobile-light.jpg");
+        $(mode).attr("src", "images/icon-moon.svg");
+
+        darkMode = false;
+    }
+
+    // Click to change themes
+    mode.click(function (e) {
+        e.preventDefault();
+
+        if (darkMode != true)
+            darkTheme()
+        else
+            lightTheme()
+    });
+
+    // Remove specific list item
+    for (var i = 0; i < removeItem.length; i++) {
+        removeItem[i].addEventListener("click", function () {
+            $(this).parent().remove();
+        });
+    }
+
+    // Click to complete a task
+    $("li").click(function (e) {
+        e.preventDefault();
+
+        if ($(this).attr("class", "incomplete")) {
+            $(this).attr("class", "completed");
+            $(this).css({
+                "text-decoration": "line-through",
+                "color": "gray"
+            })
+        } else {
+            $(this).attr("class", "incomplete");
+            $(this).css({
+                "text-decoration": "none",
+                "color": "black"
+            })
+        }
+    })
 })
