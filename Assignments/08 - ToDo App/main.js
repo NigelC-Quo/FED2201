6c33405fbadcf45b689808440b8bf5fa7e872da3
NigelC-Quo <nigelclayton2@gmail.com>
2022-02-20 21:41:18 -0500

    You can now enter a new entry into the list

--- Assignments/08 - ToDo App/main.js
+++ Assignments/08 - ToDo App/main.js
@@ -6,31 +6,58 @@ $(document).ready(() => {
     var entry = $("#toDoEntry");
     var mode = $(".theme")
     var darkMode = false;
-    var finished = false;
     var removeItem = $(".cross")
 
     // Dark theme toggle
     function darkTheme() {
         $(backImage).attr("src", "images/bg-mobile-dark.jpg");
         $(mode).attr("src", "images/icon-sun.svg");
-        $("body").css({"background-color": "hsl(235, 21%, 11%)"})
-        $(entry).css({"background-color": "hsl(235, 24%, 19%)"})
-        $("li").css({"color": "white", "background-color": "hsl(235, 24%, 19%)", "border-bottom": "1px solid hsl(234, 11%, 52%)"})
-        $("#left-clear").css({"color": "hsl(233, 14%, 35%)", "background-color": "hsl(235, 24%, 19%)"})
-        $("#tab").css({"color": "hsl(233, 14%, 35%)","background-color": "hsl(235, 24%, 19%)"})
-        
+        $("body").css({
+            "background-color": "hsl(235, 21%, 11%)"
+        })
+        $(entry).css({
+            "background-color": "hsl(235, 24%, 19%)"
+        })
+        $(active).css({
+            "color": "white",
+            "background-color": "hsl(235, 24%, 19%)",
+            "border-bottom": "1px solid hsl(234, 11%, 52%)"
+        })
+        $("#left-clear").css({
+            "color": "hsl(233, 14%, 35%)",
+            "background-color": "hsl(235, 24%, 19%)"
+        })
+        $("#tab").css({
+            "color": "hsl(233, 14%, 35%)",
+            "background-color": "hsl(235, 24%, 19%)"
+        })
+
         darkMode = true;
     }
     // Light theme toggle
     function lightTheme() {
         $(backImage).attr("src", "images/bg-mobile-light.jpg");
         $(mode).attr("src", "images/icon-moon.svg");
-        $("body").css({"background-color": "hsl(0deg 2% 95%)"})
-        $(entry).css({"background-color": "white"})
-        $("li").css({"color": "hsl(235, 19%, 35%)", "background-color": "white", "border-bottom": "1px solid hsl(233, 11%, 84%)"})
-        $("#left-clear").css({"color": "hsl(236, 9%, 61%)", "background-color": "white"})
-        $("#tab").css({"color": "hsl(236, 9%, 61%)","background-color": "white"})
-        
+        $("body").css({
+            "background-color": "hsl(0deg 2% 95%)"
+        })
+        $(entry).css({
+            "background-color": "white"
+        })
+        $(active).css({
+            "color": "hsl(235, 19%, 35%)",
+            "background-color": "white",
+            "border-bottom": "1px solid hsl(233, 11%, 84%)"
+        })
+        $("#left-clear").css({
+            "color": "hsl(236, 9%, 61%)",
+            "background-color": "white"
+        })
+        $("#tab").css({
+            "color": "hsl(236, 9%, 61%)",
+            "background-color": "white"
+        })
+
         darkMode = false;
     }
 
@@ -52,24 +79,65 @@ $(document).ready(() => {
     }
 
     // Click to complete a task or revert back to active
-    $("li").click(function (e) {
+    $("ul").on("click", "li", function (e) {
         e.preventDefault();
 
         if ($(this).hasClass("incomplete")) {
             $(this).attr("class", "completed");
-            $(this).css({
-                "text-decoration": "line-through",
-                "color": "gray"
-            })
+            if (darkMode == false)
+                $(this).css({
+                    "text-decoration": "line-through",
+                    "color": "hsl(236, 33%, 92%)"
+                })
+
+            else
+                $(this).css({
+                    "text-decoration": "line-through",
+                    "color": "hsl(233, 14%, 35%)"
+                })
         } else {
             $(this).attr("class", "incomplete");
-            $(this).css({
-                "text-decoration": "none",
-                "color": "black"
-            })
+            if (darkMode == false)
+                $(this).css({
+                    "text-decoration": "none",
+                    "color": "hsl(235, 19%, 35%)"
+                })
+
+            else
+                $(this).css({
+                    "text-decoration": "none",
+                    "color": "white"
+                })
         }
     })
 
     // Add a task
+    entry.bind("enterKey", function (e) {
 
+        alert("Task entered");
+
+        list.append(`<li class="incomplete"><img class="cross" src="images/icon-cross.svg" alt="cross">${entry.val()}</li>`);
+
+        list.on('click', '.cross', function () {
+            $(this).parent().remove();
+        });
+
+
+        entry.val("");
+    });
+
+
+    entry.keydown(function (e) {
+
+        if (e.keyCode == 13 && entry.val() !== "") {
+
+            e.preventDefault()
+            $(this).trigger("enterKey");
+
+        } else if (e.keyCode == 13 && entry.val() == "") {
+
+            e.preventDefault()
+            alert("Task can't be blank");
+        }
+    });
 })
