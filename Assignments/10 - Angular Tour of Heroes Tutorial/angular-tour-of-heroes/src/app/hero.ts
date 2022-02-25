721a43b2bfe50cdb98e5470401026f81dcc1141e
NigelC-Quo <nigelclayton2@gmail.com>
2022-02-24 19:13:33 -0500

    Added 4 more properties to the hero object

--- Assignments/10 - Angular Tour of Heroes Tutorial/angular-tour-of-heroes/src/app/hero.ts
+++ Assignments/10 - Angular Tour of Heroes Tutorial/angular-tour-of-heroes/src/app/hero.ts
@@ -1,4 +1,8 @@
 export interface Hero {
     id: number;
     name: string;
+    alterEgo: string;
+    powers: string;
+    age: number;
+    zodiac: string;
   }
--- Assignments/10 - Angular Tour of Heroes Tutorial/angular-tour-of-heroes/src/app/mock-heroes.ts
+++ Assignments/10 - Angular Tour of Heroes Tutorial/angular-tour-of-heroes/src/app/mock-heroes.ts
@@ -1,14 +1,14 @@
 import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
-  { id: 11, name: 'Dr Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
+  { id: 11, name: 'Dr Nice', alterEgo:'Lorri Street', powers: 'Unwavering positivity', age: 29, zodiac: 'Capricorn'},
+  { id: 12, name: 'Narco', alterEgo: 'Scottie Akins', powers: 'Ultimate sword-master', age: 32, zodiac: 'Sagittarius'},
+  { id: 13, name: 'Bombasto', alterEgo: 'Lang Goebel', powers: 'Bomb genius', age: 30, zodiac: 'Aries'},
+  { id: 14, name: 'Celeritas', alterEgo: 'Mitzi Foust', powers: 'Clairvoyance', age: 22, zodiac: 'Virgo'},
+  { id: 15, name: 'Magneta', alterEgo: 'Masako Means', powers: 'Potion master', age: 25, zodiac: 'Pisces'},
+  { id: 16, name: 'RubberMan', alterEgo: 'Son Bryson', powers: 'Elasticity', age: 36, zodiac: 'Gemini'},
+  { id: 17, name: 'Dynama', alterEgo: 'Tania Garland', powers: 'Lightning rod', age: 27, zodiac: 'Libra'},
+  { id: 18, name: 'Dr IQ', alterEgo: 'Demetrius Maurer', powers: 'Super intelligence', age: 24, zodiac: 'Aquarius'},
+  { id: 19, name: 'Magma', alterEgo: 'Brendon Ogden', powers: 'Fire creation', age: 21, zodiac: 'Taurus'},
+  { id: 20, name: 'Tornado', alterEgo: 'Garth Arnett', powers: 'Air manipulation', age: 35, zodiac: 'Scorpio'}
 ];
