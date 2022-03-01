c854404a15bc3cefb2cdded57a2f2135cf703682
NigelC-Quo <nigelclayton2@gmail.com>
2022-03-01 15:04:09 -0500

    Added decks component

--- Projects/Ecommerce/angular-ecommerce/src/app/app.module.ts
+++ Projects/Ecommerce/angular-ecommerce/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DecksComponent } from './decks/decks.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DecksComponent
   ],
   imports: [
     BrowserModule,
---
+++ Projects/Ecommerce/angular-ecommerce/src/app/decks/decks.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-decks',
+  templateUrl: './decks.component.html',
+  styleUrls: ['./decks.component.sass']
+})
+export class DecksComponent implements OnInit {
+
+  decks = "Green Xtreme"
+  
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
