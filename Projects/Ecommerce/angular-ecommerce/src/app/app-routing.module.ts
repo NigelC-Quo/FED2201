38e4530cd9fb3528b416f8cbf5c77a5d134052ec
NigelC-Quo <nigelclayton2@gmail.com>
2022-03-01 16:03:51 -0500

    Added routing

--- Projects/Ecommerce/angular-ecommerce/src/app/app-routing.module.ts
+++ Projects/Ecommerce/angular-ecommerce/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DecksComponent } from './decks/decks.component';
+import { TrucksComponent } from './trucks/trucks.component';
+import { WheelsComponent } from './wheels/wheels.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'decks', component: DecksComponent },
+  { path: 'trucks', component: TrucksComponent },
+  { path: 'wheels', component: WheelsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
