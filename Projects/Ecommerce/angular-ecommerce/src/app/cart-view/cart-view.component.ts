ec3048ed7a80cfa366a764c8bcd5acd63620fdc1
NigelC-Quo <nigelclayton2@gmail.com>
2022-03-20 01:40:56 -0400

    Added total order details

--- Projects/Ecommerce/angular-ecommerce/src/app/cart-view/cart-view.component.ts
+++ Projects/Ecommerce/angular-ecommerce/src/app/cart-view/cart-view.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SkateService } from '../skate.service';
 
 @Component({
   selector: 'app-cart-view',
@@ -7,7 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CartViewComponent implements OnInit {
 
-  constructor() { }
+  items = this.skateService.getItems();
+
+
+  constructor(
+    private skateService: SkateService,
+  ) { }
+  
 
   ngOnInit(): void {
   }
