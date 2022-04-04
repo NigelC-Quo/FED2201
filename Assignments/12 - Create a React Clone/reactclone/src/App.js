9355e3791d9df77167f560cc91992848e3859b16
NigelC-Quo <nigelclayton2@gmail.com>
2022-04-04 06:08:31 -0400

    Added version component

--- Assignments/12 - Create a React Clone/reactclone/src/App.js
+++ Assignments/12 - Create a React Clone/reactclone/src/App.js
@@ -3,12 +3,14 @@ import Logo from './components/Logo';
 import BGImage from './images/aztezgameplay.jpg';
 import Caption from './components/Caption';
 import ButtonPrompt from './components/ButtonPrompt';
+import Version from './components/Version';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
      <img className="bg" src={BGImage} alt="aztezBG"/>
+     <Version/>
      <hr className='topBar'/>
      <Logo/>
      <hr/>
---
+++ Assignments/12 - Create a React Clone/reactclone/src/components/Version.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function ver() {
+    const version = <h2 className="ver">1.2-AEEB620</h2>
+    
+    return ( 
+        <div>
+        {version}
+        </div>
+    );
+}
+
+export default ver;
